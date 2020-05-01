c4bf65ad4c6b2d038581031d909b72371693147f
amgillenwater <amgillenwater@gmail.com>
2020-05-01 13:54:36 -0400

    working on validate name, got the function written. mostly working, still needs to return invalid for no string

--- script.js
+++ script.js
@@ -3,9 +3,26 @@ let form = document.querySelector('#parking-form')
 
 form.addEventListener('submit', function (event) {
   event.preventDefault()
+//all validation functions will go here
+//need to add something that resets everything to invalid unless there is a value in it, right now just submitting a blank page will return that the name is valid
+  validateName()
+
 })
 
-removeErrorMessage()
-formIsValid = true
-//these two give us a "clean slate" to work from--not quite sure the actual mechanics of this, come back and learn more later
+function validateName () {
+let nameInput = document.querySelector('#name');
+let nameValue = nameInput.value;
+let parentName = nameInput.parentElement;
+    if (nameInput){
+        console.log("Name is valid")
+        parentName.classList.remove('input-invalid')
+        parentName.classList.add('input-valid')
+    } else {
+        console.log("Name is not valid")
+        parentName.classList.remove('input-valid')
+        parentName.classList.add('input-invalid')
+        formIsValid=false
 
+    }
+}
+//need to figure out how to tell it if there is an alphabetical string there, it is valid
