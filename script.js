7d4eca49b7e17964836e4e01c9e5c7d84ad6bf82
amgillenwater <amgillenwater@gmail.com>
2020-05-01 11:32:18 -0400

    set up the form with prevent default

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Add validation!');
+  
+let form = document.querySelector('#parking-form')
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+})
+
+removeErrorMessage()
+formIsValid = true
+//these two give us a "clean slate" to work from--not quite sure the actual mechanics of this, come back and learn more later
+
