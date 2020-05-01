b5d864707496e1fc6b3204571f8c744cf8b1ce90
amgillenwater <amgillenwater@gmail.com>
2020-05-01 15:18:47 -0400

    made an update to the name field to accept special characters

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
   
 let form = document.querySelector('#parking-form')
 let formIsValid
-let alphabetic = /^[A-Za-z]+$/
+let alphabetic = /^[A-Za-z\'\s\.\,]+$/
+//this is an expression that means only letters of the alphabet, declared the alphabetic variable to use as a condition in validation if/else statements, found here: https://www.w3resource.com/javascript/form/all-letters-field.php
 
 form.addEventListener('submit', function (event) {
     formIsValid = true
     event.preventDefault()
-//need to add something that resets everything to invalid unless there is a value in it, right now just submitting a blank page will return that the name is valid
+//need to add something that resets everything to invalid unless there is a value in it, right now just submitting a blank page will return that the name is valid--a note: this might not be a global type of thing but needs to be included in each validation
 //all validation functions will go here
     validateName()
 
@@ -17,6 +18,7 @@ let nameInput = document.querySelector('#name');
 let nameValue = nameInput.value;
 let nameLength = nameValue.length;
 let parentName = nameInput.parentElement;
+//if the string has at least one charcter in it and all of it's characters are alphabetic, the name is valid, if not it is invalid
     if (nameLength >= 1 && nameValue.match(alphabetic)){
         console.log("Name is valid")
         parentName.classList.remove('input-invalid')
@@ -29,5 +31,3 @@ let parentName = nameInput.parentElement;
 
     }
 }
-//need to figure out how to tell it the string needs to be alphabetic characters
-
