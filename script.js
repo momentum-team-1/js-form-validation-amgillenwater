bea1603efc6244071f5837b6e797a597be139cc1
amgillenwater <amgillenwater@gmail.com>
2020-05-13 20:16:44 -0400

    fixed the duplicating error

--- script.js
+++ script.js
@@ -1,18 +1,61 @@
-//stuck on validating mm-dd-yyy
-//need to figure out how to make error alert box go away when refreshing, right now they just pile up!   
-
-
-
 let form = document.querySelector('#parking-form')
 let formIsValid
 let alphabetic = /^[A-Za-z\'\s\.\,]+$/
+
+let nameAlert = document.createElement("div");
+let parentName = document.getElementById("name-field")
+nameAlert.className = "error"
+parentName.appendChild(nameAlert);
+
+let carAlert = document.createElement("div")
+let parentCar = document.getElementById("car-field")
+carAlert.className = "error"
+parentCar.appendChild(carAlert);
+
+let carMakeAlert = document.createElement("div")
+carMakeAlert.className = "error"
+parentCar.appendChild(carMakeAlert)
+
+let carModelAlert = document.createElement("div")
+carModelAlert.className = "error"
+parentCar.appendChild(carModelAlert)
+
+let dateAlert = document.createElement("div");
+let parentDate = document.getElementById("start-date-field")
+dateAlert.className = "error"
+parentDate.appendChild(dateAlert);
+
+let numDaysAlert = document.createElement("div");
+let parentNumDays = document.getElementById("days-field")
+numDaysAlert.className = "error"
+parentNumDays.appendChild(numDaysAlert);
+
+let creditAlert = document.createElement("div");
+let parentCredit = document.getElementById("credit-card-field")
+creditAlert.className = "error"
+parentCredit.appendChild(creditAlert);
+
+let cvvAlert = document.createElement("div");
+let parentCvv = document.getElementById("cvv-field")
+cvvAlert.className = "error"
+parentCvv.appendChild(cvvAlert);
+
+let expiryAlert = document.createElement("div");
+let parentExpiry = document.getElementById("expiration-field")
+expiryAlert.className = "error"
+parentExpiry.appendChild(expiryAlert);
+
+
+
+// let parentDate = document.getElementById("start-date-field")
+// parentDate.appendChild(alert);
+// alert.innerHTML=""
 //this is an expression that means only letters of the alphabet, declared the alphabetic variable to use as a condition in validation if/else statements, found here: https://www.w3resource.com/javascript/form/all-letters-field.php
+
+
 
 form.addEventListener('submit', function (event) {
     formIsValid = true
-    event.preventDefault()
-//need to add something that resets everything to invalid unless there is a value in it, right now just submitting a blank page will return that the name is valid--a note: this might not be a global type of thing but needs to be included in each validation
-//all validation functions will go here
     validateName();
     validateCarYear();
     validateCarMake();
@@ -22,33 +65,28 @@ form.addEventListener('submit', function (event) {
     validateDate();
     validateCvv();
     validateExp();
+    event.preventDefault()
 
 })
-
-// function removeErrorMessage () {
-//     let errorDiv = document.querySelector('#errordiv')
-//     errorDiv.innerHTML = ''
-// }
 
 function validateName () {
 let nameInput = document.querySelector('#name');
 let nameValue = nameInput.value;
 let nameLength = nameValue.length;
-let parentName = nameInput.parentElement;
-let nameAlert = document.createElement("div")
+
 //if the string has at least one charcter in it and all of it's characters are alphabetic, the name is valid, if not it is invalid
     if (nameLength >= 1 && nameValue.match(alphabetic)){
         console.log("Name is valid")
         parentName.classList.remove('input-invalid')
         parentName.classList.add('input-valid')
-        nameAlert.textContent = ""
+        nameAlert.innerHTML = ""
+
     } else {
         console.log("Name is not valid")
         parentName.classList.remove('input-valid')
         parentName.classList.add('input-invalid')
         formIsValid=false
-        nameAlert.textContent = "Valid name is required"
-        document.getElementById("name-field").appendChild(nameAlert)
+        nameAlert.innerHTML = "Name is not valid"
     }
 }
 
@@ -58,18 +96,17 @@ function validateCarYear () {
     let yearValue = yearInput.value;
     let yearLength = yearValue.length;
     let yearParent = yearInput.parentElement;
-    let carAlert = document.createElement("div")
 //is there a way to have a dynamic date? 2021 model year cars come out soon and the way i have done it would need to be manually updated each year :/
     if (yearLength === 4 && yearValue >= 1900 && yearValue <= 2020) {
         console.log("Year is valid");
         yearParent.classList.remove ('input-invalid');
         yearParent.classList.add ('input-valid');
+        carAlert.innerHTML = ""
     } else {
         console.log("Year is invalid");
         yearParent.classList.remove('input-valid');
         yearParent.classList.add('input-invalid');
-        carAlert.textContent = "Valid car year is required"
-        document.getElementById("car-field").appendChild(carAlert)
+        carAlert.innerHTML = "Car Year is invalid"
     }
 //needs more validation for a range of years
 }
@@ -79,19 +116,19 @@ function validateCarMake () {
     let makeValue = makeInput.value;
     let makeLength = makeValue.length;
     let makeParent = makeInput.parentElement;
-    let carAlert = document.createElement("div")
 
     if (makeLength >= 1){
         console.log ("Make is valid");
         makeParent.classList.remove('input-invalid');
         makeParent.classList.add
         ('input-valid');
+        carMakeAlert.innerHTML = ""
     } else {
         console.log("Make is invalid");
         makeParent.classList.remove ('input-valid')
         makeParent.classList.add('input-invalid')
-        carAlert.innerText = "Valid car make is required"
-        document.getElementById("car-field").appendChild(carAlert)
+        carMakeAlert.innerHTML = "Car Make is invalid"
+
     }
 }
 
@@ -100,19 +137,19 @@ function validateCarModel () {
     let modelValue = modelInput.value;
     let modelLength = modelValue.length;
     let modelParent = modelInput.parentElement;
-    let carAlert = document.createElement("div")
 
     if (modelLength >= 1){
         console.log ("Model is valid");
         modelParent.classList.remove('input-invalid');
         modelParent.classList.add
         ('input-valid');
+        carModelAlert.innerHTML = ""
     } else {
         console.log("Model is invalid");
         modelParent.classList.remove ('input-valid')
         modelParent.classList.add('input-invalid')
-        carAlert.innerText = "Valid car model is required"
-        document.getElementById("car-field").appendChild(carAlert)
+        carModelAlert.innerHTML = "Car Model is invalid"
+
     }
 }
 
@@ -123,19 +160,19 @@ function validateDate (){
     dateValue = new Date(dateValue);
     let parkDate = dateInput.valueAsNumber;
     let dateParent = dateInput.parentElement;
-    let dateAlert = document.createElement("div");
 
     if (parkDate >= todayDate){
         console.log("Date is valid")
         dateParent.classList.remove('input-invalid');
         dateParent.classList.add
         ('input-valid');
+        dateAlert.innerHTML = ""
     } else {
         console.log("Date is invalid")
         dateParent.classList.remove ('input-valid')
         dateParent.classList.add('input-invalid')
-        dateAlert.innerText = "Pick a date in the future."
-        document.getElementById("start-date").appendChild(dateAlert)
+        dateAlert.innerHTML = "Pick a date in the future"
+
     }
 }
 
@@ -144,19 +181,17 @@ function validateNumDays () {
     let daysInput = document.querySelector('#days');
     let daysValue = daysInput.value;
     let parentName = daysInput.parentElement;
-    let daysAlert = document.createElement("div");
 
         if (daysValue > 0 && daysValue < 31) {
             console.log("Days are valid")
             parentName.classList.add('input-valid')
-            daysAlert.innerText = ""
+            numDaysAlert.innerText = ""
         } else {
             console.log("Days are not valid")
             parentName.classList.remove('input-valid')
             parentName.classList.add('input-invalid')
             formIsValid=false
-            daysAlert.innerText = "You can only reserve parking for up to 30 days."
-            document.getElementById("days-field").appendChild(daysAlert)
+            numDaysAlert.innerText = "You must reserve parking for 1 to 30 days."
         }
 }
 
@@ -164,7 +199,6 @@ function validateCC(){
     let cardInput = document.querySelector('#credit-card')
     let cardValue = cardInput.value
     let parentCard = cardInput.parentElement
-    let cardAlert = document.createElement("div");
     let cardRegex = new RegExp ('^[0-9]{16}$')
     document.getElementsByTagName('label')[4].setAttribute('id', 'CC-label')
     let CCLabel = document.querySelector('#CC-Label')
@@ -172,14 +206,15 @@ function validateCC(){
     if(cardValue && cardRegex.test(cardValue) && luhnCheck(cardValue)){
         console.log("credit card is valid")
         parentCard.classList.add('input-valid')
+        creditAlert.innerText = ""
+
 
     }else{
         console.log("credit card number is not valid")
         parentCard.classList.remove('input-valid')
         parentCard.classList.add('input-invalid')
         formIsValid=false
-        cardAlert.innerText = "Please enter a valid credit card number"
-        document.getElementById("credit-card-field").appendChild(cardAlert)
+        creditAlert.innerText = "Please enter a valid credit card number"
     }
    
 } 
@@ -204,12 +239,12 @@ function validateCvv(){
     let cvvValue = cvvInput.value;
     let cvvLength = cvvValue.length;
     let parentCvv = cvvInput.parentElement;
-    let cvvAlert = document.createElement("div")
 
     if (cvvLength === 3){
         console.log("Valid CVV")
         parentCvv.classList.remove("input-invalid")
         parentCvv.classList.add('input-valid')
+        cvvAlert.innerText = ""
 
     } else {
         console.log("CVV is invalid")
@@ -217,27 +252,9 @@ function validateCvv(){
         parentCvv.classList.add('input-invalid')
         formIsValid=false
         cvvAlert.innerText = "CVV is invalid"
-        document.getElementById("cvv-field").appendChild(cvvAlert)
-
     }
 }
-// let dateInput = document.querySelector("#start-date");
-// let dateValue = dateInput.value;
 
-// let daysInput = document.querySelector('#days');
-// let daysValue = daysInput.value;
-
-// let experiment = moment(dateValue).add(daysValue, 'days');
-// console.log(experiment);
-// function cost(){
-//     // let test = new Date();
-//     // document.getElementById("#start-date").innerHTML = test.getDay();
-
-//     // console.log(test)
-//     //use the getDay function to get the index of the day
-//     //use the index of the day to multiply by that day's rate (for instance, 1-5 will be $5 so you can multiply those by 5., for indexes of 0 and 6, multiply by 7)
-//     //Add the results of step 2 together (this will repeat for x number of days in stored in #days (use days.value?)  on the form)
-// }
 function validateExp() {
     let expInput = document.querySelector("#expiration")
     let expValue = expInput.value 
@@ -255,14 +272,14 @@ function validateExp() {
         console.log("Expiration Date valid")
         parentExp.classList.remove("input-invalid")
         parentExp.classList.add('input-valid')
+        expiryAlert.innerHTML = ""
+
 
     } else {
         console.log("Expiration Date invalid")
         parentExp.classList.remove("input-valid")
         parentExp.classList.add('input-invalid')
         formIsValid=false
-        expAlert.innerText = "Expiration date is invalid"
-        document.getElementById("expiration-field").appendChild(expAlert)
-        
+        expiryAlert.innerHTML = "Expiration date is invalid"
     }
 }
